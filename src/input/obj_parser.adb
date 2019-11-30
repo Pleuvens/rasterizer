b553e323c9c9c936e7109281593dce2b8c9c01fc
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2019-11-30 07:34:26 +0100

    [OBJ PARSER] - reading input file line by line

--- src/input/obj_parser.adb
+++ src/input/obj_parser.adb
@@ -7,8 +7,8 @@ with Scene; use Scene;
 package body OBJ_Parser is
 
     function Read_3_Float(Line: String;
-                           V: in out Vector.Vector) return Boolean is
-        I, I1, I2, I3: Natural := 0;
+        V: in out Vector.Vector) return Boolean is
+        I, I1, I2, I3: Natural := Line'First;
     begin
         while I < Line'Last and Line(I) /= ' ' loop
             I := I + 1;
@@ -17,6 +17,7 @@ package body OBJ_Parser is
             return false;
         end if;
         I1 := I;
+        I := I + 1;
         while I < Line'Last and Line(I) /= ' ' loop
             I := I + 1;
         end loop;
@@ -24,13 +25,13 @@ package body OBJ_Parser is
             return false;
         end if;
         I2 := I;
+        I := I + 1;
         while I < Line'Last and Line(I) /= ' ' loop
             I := I + 1;
-        end loop;
-        if I = Line'Last then
-            return false;
-        end if;
+        end loop; 
         I3 := I;
+        Put_Line(Line & " " & Line((I1 + 1) .. (I2 - 1)) & " :"
+        & Natural'Image(I1 + 1) & " " & Natural'Image(I2 - 1));
         Vector.Vector_Set(V, 1, Float'Value(Line((I1 + 1) .. (I2 - 1))));
         Vector.Vector_Set(V, 2, Float'Value(Line((I2 + 1) .. (I3 - 1))));
         Vector.Vector_Set(V, 3, Float'Value(Line((I3 + 1) .. Line'Last)));
@@ -38,10 +39,9 @@ package body OBJ_Parser is
     end Read_3_Float;
 
     function Parse(File_Name: String;
-                   S: out Scene.Scene) return Boolean is
+        S: out Scene.Scene) return Boolean is
         Nb_V, Nb_Vn, Nb_G, Nb_f, I, Tr_Cnt : Natural := 0;
         File: File_Type;
-        Line: String(1 .. 2);
         V, Vn : Vector.Vector;
         Vs, Vns : Vector_Arr(1 .. 65000);
         C: Color.Color := (0.0, 0.0, 0.0);
@@ -50,40 +50,47 @@ package body OBJ_Parser is
         Mode => In_File,
         Name => File_Name);
         while not  End_Of_File (File) Loop
-            Put_Line(Get_Line (File));
-            --if Line(1) = '#' then
-            --    goto Continue;
-            --end if;
-            if false then
-            V := Vector.Vector_Create(0.0, 0.0, 0.0);
-            if Line(Line'First..(Line'First + 1)) = "vn" and Read_3_Float(Line, V) then
-                Vs(Nb_V) := V;
-                Nb_V := Nb_V + 1;
-            elsif Line(Line'First) = 'v' and Read_3_Float(Line, Vn) then
-                Vns(Nb_Vn) := Vn;
-                Nb_Vn := Nb_Vn + 1;
-            --elsif Line(1) = 'g' then
-                --Scene_Add_Group(S, G);
-            --elsif Line(1) = 'f' then
-                --Scene_Add_Face(S);
-            end if;
-        end if;
+            declare
+                Line : String := Get_Line(File);
+            begin
+                --if Line(1) = '#' then
+                --    goto Continue;
+                --end if;
+                V := Vector.Vector_Create(0.0, 0.0, 0.0);
+                if Line'Last > Line'First then 
+                    if Line(Line'First..(Line'First + 1)) = "vn"
+                        and Read_3_Float(Line, V)
+                    then
+                        Vs(Nb_V) := V;
+                        Nb_V := Nb_V + 1; 
+                    elsif Line(Line'First) = 'v' and Read_3_Float(Line, Vn)
+                    then
+                        Vns(Nb_Vn) := Vn;
+                        Nb_Vn := Nb_Vn + 1;
+                        --elsif Line(1) = 'g' then
+                        --Scene_Add_Group(S, G);
+                        --elsif Line(1) = 'f' then
+                        --Scene_Add_Face(S);
+                    end if;
+                end if;
+            end;
         end loop;
         Close (File);
+        Put_Line(Natural'Image(Nb_V));
         if false then
-        S := Scene.Scene_Create(128, 128, Nb_V / 3, (0.0, 0.0, 0.0));
-        if Nb_V /= Nb_Vn or Nb_V mod 3 /= 0 then
-            return false;
-        end if;
+            S := Scene.Scene_Create(128, 128, Nb_V / 3, (0.0, 0.0, 0.0));
+            if Nb_V /= Nb_Vn or Nb_V mod 3 /= 0 then
+                return false;
+            end if;
 
-        while I < Nb_V loop
-            Scene_Set_Triangle(S, Tr_Cnt, Triangle.Triangle_Create((Vs(I),
-            Vs(I + 1), Vs(I + 2)),
-            (Vns(I), Vns(I + 1), Vns(I + 2)), (C, C, C)));
-            I := I + 3;
-            Tr_Cnt := Tr_Cnt + 1;
-        end loop;
-    end if;
+            while I < Nb_V loop
+                Scene_Set_Triangle(S, Tr_Cnt, Triangle.Triangle_Create((Vs(I),
+                Vs(I + 1), Vs(I + 2)),
+                (Vns(I), Vns(I + 1), Vns(I + 2)), (C, C, C)));
+                I := I + 3;
+                Tr_Cnt := Tr_Cnt + 1;
+            end loop;
+        end if;
         return true;
     end Parse;
 
